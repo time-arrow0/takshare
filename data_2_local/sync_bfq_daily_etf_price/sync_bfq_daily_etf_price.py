edf5e971a76586f897ef3d7550d919b03d56670e
ninja <work_ninja@163.com>
2026-01-30 17:37:18 +0800

    fix: etf每日同步，去掉通达信同步

--- data_2_local/sync_bfq_daily_etf_price/sync_bfq_daily_etf_price.py
+++ data_2_local/sync_bfq_daily_etf_price/sync_bfq_daily_etf_price.py
@@ -81,101 +81,6 @@ def get_file_data(code, greater_date_ts=None):
         df = df[df['date'] > greater_date_ts]
     return df
 
-def initial_tdx_file_data_2_local():
-    with open(file='data/bfq_daily_etf_price/codes.txt', mode='r', encoding='utf-8') as f:
-        content = f.read()
-        etf_codes0 = content.split('\n')
-        etf_codes = []
-        for code in etf_codes0:
-            if code.strip():
-                etf_codes.append(code)
-    with open(file='data/bfq_daily_etf_price/complete_codes.txt', mode='r', encoding='utf-8') as f:
-        content = f.read()
-        complete_codes0 = content.split('\n')
-        complete_codes = []
-        for code in complete_codes0:
-            if code.strip():
-                complete_codes.append(code)
-    complete_code_set = set(complete_codes)
-    codes = []
-    for code in etf_codes:
-        if code in complete_code_set:
-            continue
-        codes.append(code)
-    # t_codes = ['002003']
-    print(f'len_codes: {len(codes)}')
-
-    for code in codes:
-        df = get_file_data(code, None)
-        df_append_2_local(table_name=table_name, df=df)
-        with open(file='data/bfq_daily_etf_price/complete_codes.txt', mode='a', encoding='utf-8') as f:
-            f.write(f'{code}\n')
-
-def tdx_file_data_2_local(tdx_file_dir, start_date_str=None, end_date_str=None):
-    # 逐个读取目标目录文件，查表中该代码最大日期，把大于最大日期的数据写入数据库
-    items = os.listdir(tdx_file_dir)
-    dtype_dict = {
-        'open': 'float64',
-        'close': 'float64',
-        'high': 'float64',
-        'low': 'float64',
-        'turnover': 'float64',
-    }
-    skiprows = 2
-    # turnover: 成交额
-    column_names = ['date', 'open', 'high', 'low', 'close', 'volume', 'turnover']
-    # 代码在文件名中的截取范围
-    code_start = 7
-    code_end = 13  # 不包含
-    # 已完成的代码
-    with open(file='data/bfq_daily_etf_price/complete_codes.txt', mode='r', encoding='utf-8') as f:
-        content = f.read()
-        complete_codes = content.split('\n')
-    complete_code_set = set()
-    for code in complete_codes:
-        if code.strip():
-            complete_code_set.add(code)
-
-    for item in items:
-        code = item[code_start: code_end]
-        if code in complete_code_set:
-            continue
-        try:
-            file_path = os.path.join(tdx_file_dir, item)
-            df = pd.read_csv(file_path,
-                         sep='\s+',
-                         skiprows=skiprows,
-                         comment='#',  # 跳过以#开头的行
-                         skip_blank_lines=True,  # 跳过空行
-                         names=column_names,
-                         dtype=dtype_dict,
-                         encoding='GBK')
-            df = df[['date', 'open', 'high', 'low', 'close', 'volume', 'turnover']]
-            df['code'] = code
-            df['date'] = pd.to_datetime(df['date'])
-            last_date = get_price_last_date(table_name, code)
-            if last_date is not None:
-                last_date_ts = pd.Timestamp(last_date)
-                df = df[df['date'] > last_date_ts].copy()
-                if start_date_str:
-                    start_date_ts = pd.Timestamp(start_date_str)
-                    df = df[df['date'] >= start_date_ts].copy()
-                if end_date_str:
-                    end_date_ts = pd.Timestamp(end_date_str)
-                    df = df[df['date'] <= end_date_ts].copy()
-
-            if df.shape[0] > 0:
-                df_append_2_local(table_name=table_name, df=df)
-            with open(file='data/bfq_daily_etf_price/complete_codes.txt', mode='a',
-                      encoding='utf-8') as f:
-                f.write(f'{code}\n')
-            complete_code_set.add(code)
-        except Exception as e:
-            s = f'{code}保存出错'
-            LOGGER.error(s)
-            raise e
-
-
 
 def daily_data_2_local():
     """
@@ -342,8 +247,6 @@ if __name__ == '__main__':
     pd.set_option('display.width', 1000)  # 设置列宽
     pd.set_option('display.colheader_justify', 'left')  # 设置列标题靠左
 
-    # initial_tdx_file_data_2_local()
-    # tdx_file_data_2_local('D:/new_tdx/T0002/export/bfq-etf-20260113', start_date_str=None, end_date_str='20260112')
     try:
         daily_data_2_local()
     except Exception as e:
